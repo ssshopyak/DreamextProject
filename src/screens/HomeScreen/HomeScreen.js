bc3acffc25ecfa1c7bdb9330de9dab88eabcb96a
Ostap Shopyak <90937268+ssshopyak@users.noreply.github.com>
2023-01-04 11:51:15 +0200

    feature(post list + comments)

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   View,
@@ -8,11 +9,60 @@ import {
   FlatList,
 } from 'react-native';
 import {styles} from './style';
+import Modal from 'react-native-modal';
 
 export const HomeScreen = ({route}) => {
+  const [posts, setPosts] = useState({});
+  const [comments, setComments] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
+
   const {toAuthorize} = route.params;
+
+  const fetchCommentsById = id => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/comments?postId=' + id)
+      .then(res => {
+        setComments(res.data);
+      });
+  };
+
+  const renderModal = id => {
+    setModalVisible(true);
+    fetchCommentsById(id);
+  };
+
+  const renderPostItem = ({item}) => (
+    <TouchableOpacity
+      onPress={() => {
+        renderModal(item.id);
+      }}>
+      <View style={styles.postsContainer}>
+        <Text style={{padding: 5}}>title: {item.title}</Text>
+        <Text style={{padding: 5}}>body: {item.body}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
+  const renderCommentItem = ({item}) => (
+    <TouchableOpacity
+      onPress={() => {
+        renderModal(item.id);
+      }}>
+      <View style={styles.postsContainer}>
+        <Text style={{padding: 5}}>name: {item.name}</Text>
+        <Text style={{padding: 5}}>body: {item.body}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+      setPosts(res.data);
+    });
+  }, []);
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.logo}>SHEVA</Text>
         <TouchableOpacity onPress={toAuthorize}>
@@ -22,6 +72,30 @@ export const HomeScreen = ({route}) => {
           />
         </TouchableOpacity>
       </View>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{marginTop: 70}}
+        data={posts}
+        renderItem={renderPostItem}
+        keyExtractor={item => item.id}
+      />
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <FlatList
+              data={comments}
+              renderItem={renderCommentItem}
+              keyExtractor={item => item.id}
+            />
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
--- src/screens/HomeScreen/style.js
+++ src/screens/HomeScreen/style.js
@@ -2,11 +2,14 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'rgb(232, 240, 255)',
   },
   header: {
+    position: 'absolute',
+    top: 0,
+    zIndex: 1,
     alignItems: 'center',
     justifyContent: 'space-between',
     flexDirection: 'row',
@@ -25,5 +28,37 @@ export const styles = StyleSheet.create({
     marginHorizontal: 10,
     fontSize: 24,
     color: 'rgba(0,0,0,0.7)',
+  },
+  postsContainer: {
+    width: '100%',
+    backgroundColor: 'rgb(181, 208, 255)',
+    borderRadius: 25,
+    padding: 10,
+    marginTop: 5,
+  },
+  centeredView: {
+    maxHeight: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 10,
+    backgroundColor: 'white',
+    padding: 5,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
